2bde35c501fee0d3bb1a3dcf29a1639c6d735ac6
Alex Henderson <alex.hendo25@gmail.com>
2021-10-23 00:37:15 +1100

    Added discrepancy count to CSV file

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def main():
     except:
         print('Error: Something with the CSV, fuck if I know what')
     
-    field = ['coin', 'margin', 'class', 'buy_price', 'sell_price', 'buy_exchange', 'sell_exchange', 'time_found']
+    field = ['coin', 'margin', 'class', 'buy_price', 'sell_price', 'buy_exchange', 'sell_exchange', 'time_found', 'discrepancy_no']
 
     with open('output{}.csv'.format(start_time2), 'w', encoding = 'UTF8') as f:
         writer = csv.DictWriter(f, fieldnames = field)
@@ -37,10 +37,9 @@ def main():
     fn.checkstatus_coinspot()
     fn.checkstatus_coinjar()
     fn.checkstatus_swiftx()
-    print('\n')
     
     while start == 1:
-        question = input('API Status OK, begin searching? Y/N\n')
+        question = input('\nAPI Status OK, begin searching? Y/N\n')
         if question == 'y' or question == 'Y':
             print('\nNow Searching...','\n')
             print('Search Initiated at:', start_time,'\n')
@@ -70,7 +69,7 @@ def main():
         btc_sell.sort(reverse = True, key=sort)
         
         #Check variable calls check_margin from functions and saves True or False depending on whether margin is larger than 0.1
-        # check_btc = fn.check_margin(btc_buy[0]['price'], btc_sell[0]['price'])[0]
+        check_btc = fn.check_margin(btc_buy[0]['price'], btc_sell[0]['price'])[0]
         
         #Margin variable calls check_margin from functions and saves the profit margin
         margin_btc = fn.check_margin(btc_buy[0]['price'], btc_sell[0]['price'])[1]
@@ -88,7 +87,7 @@ def main():
         xrp_sell = [fn.getprice_coinspot('xrp', 'bid'), fn.getprice_swiftx('XRP', 'sell'), fn.getprice_coinjar('XRP', 'bid')]
         xrp_sell.sort(reverse= True, key=sort)
         
-        # check_xrp = fn.check_margin(xrp_buy[0]['price'], xrp_sell[0]['price'])[0]
+        check_xrp = fn.check_margin(xrp_buy[0]['price'], xrp_sell[0]['price'])[0]
         
         margin_xrp = fn.check_margin(xrp_buy[0]['price'], xrp_sell[0]['price'])[1]
         exchange_xrp = {'Buy': xrp_buy[0]['exchange'], 'Sell': xrp_sell[0]['exchange']}
@@ -103,7 +102,7 @@ def main():
         ltc_sell = [fn.getprice_coinspot('ltc', 'bid'), fn.getprice_swiftx('LTC', 'sell'), fn.getprice_coinjar('LTC', 'bid')]
         ltc_sell.sort(reverse= True, key = sort)
         
-        # check_ltc = fn.check_margin(ltc_buy[0]['price'], ltc_sell[0]['price'])[0]
+        check_ltc = fn.check_margin(ltc_buy[0]['price'], ltc_sell[0]['price'])[0]
         
         margin_ltc = fn.check_margin(ltc_buy[0]['price'], ltc_sell[0]['price'])[1]
         exchange_ltc = {'Buy': ltc_buy[0]['exchange'], 'Sell': ltc_sell[0]['exchange']}
@@ -118,7 +117,7 @@ def main():
         xlm_sell = [fn.getprice_coinspot('str', 'bid'), fn.getprice_swiftx('XLM', 'sell'), fn.getprice_coinjar('XLM', 'bid')]
         xlm_sell.sort(reverse= True, key=sort)
         
-        # check_xlm = fn.check_margin(xlm_buy[0]['price'], xlm_sell[0]['price'])[0]
+        check_xlm = fn.check_margin(xlm_buy[0]['price'], xlm_sell[0]['price'])[0]
         
         margin_xlm = fn.check_margin(xlm_buy[0]['price'], xlm_sell[0]['price'])[1]
         exchange_xlm = {'Buy': xlm_buy[0]['exchange'], 'Sell': xlm_sell[0]['exchange']}
@@ -133,7 +132,7 @@ def main():
         ada_sell = [fn.getprice_coinspot('ada', 'bid'), fn.getprice_swiftx('ADA', 'sell')]
         ada_sell.sort(reverse= True, key=sort)
         
-        # check_ada = fn.check_margin(ada_buy[0]['price'], ada_sell[0]['price'])[0]
+        check_ada = fn.check_margin(ada_buy[0]['price'], ada_sell[0]['price'])[0]
         
         margin_ada = fn.check_margin(ada_buy[0]['price'], ada_sell[0]['price'])[1]
         exchange_ada = {'Buy': ada_buy[0]['exchange'], 'Sell': ada_sell[0]['exchange']}
@@ -145,6 +144,22 @@ def main():
                 writer = csv.DictWriter(f, fieldnames = field)
                 writer.writerows(rows)
         
+        if check_btc == True:
+            moderate_discrepancy +=1
+        
+        if check_xrp == True: 
+            moderate_discrepancy +=1
+        
+        if check_ltc == True:
+            moderate_discrepancy +=1
+        
+        if check_xlm == True:
+            moderate_discrepancy +=1
+        
+        if check_ada == True:
+            moderate_discrepancy +=1
+
+        
         #Format Discrepancy Data into usable dictionaries
         rows_btc = [{
             'coin': 'BTC',
@@ -154,7 +169,8 @@ def main():
             'sell_price': btc_sell[0]['price'],
             'buy_exchange': exchange_btc['Buy'],
             'sell_exchange': exchange_btc['Sell'],
-            'time_found': current_time
+            'time_found': current_time,
+            'discrepancy_no': moderate_discrepancy
             }]
         
         rows_xrp = [{
@@ -165,7 +181,8 @@ def main():
             'sell_price': xrp_sell[0]['price'],
             'buy_exchange': exchange_xrp['Buy'],
             'sell_exchange': exchange_xrp['Sell'],
-            'time_found': current_time
+            'time_found': current_time,
+            'discrepancy_no': moderate_discrepancy
             }]
         
         rows_ltc = [{
@@ -176,7 +193,8 @@ def main():
             'sell_price': ltc_sell[0]['price'],
             'buy_exchange': exchange_ltc['Buy'],
             'sell_exchange': exchange_ltc['Sell'],
-            'time_found': current_time
+            'time_found': current_time,
+            'discrepacny_no': moderate_discrepancy
             }]
 
         rows_xlm = [{
@@ -187,7 +205,8 @@ def main():
             'sell_price': xlm_sell[0]['price'],
             'buy_exchange': exchange_xlm['Buy'],
             'sell_exchange': exchange_xlm['Sell'],
-            'time_found': current_time
+            'time_found': current_time,
+            'discrepacny_no': moderate_discrepancy
             }]
        
         rows_ada = [{
@@ -198,16 +217,16 @@ def main():
             'sell_price': ada_sell[0]['price'],
             'buy_exchange': exchange_ada['Buy'],
             'sell_exchange': exchange_ada['Sell'],
-            'time_found': current_time
+            'time_found': current_time,
+            'discrepancy_no': moderate_discrepancy
             }]
         
         # Check Profit Margin data to determine whether a discrepany has been found and
         # calls the csv write function with the data stored in the given pricing dictionary at that time
-        
+
         if margin_btc >= 0.5:
             csv_row(rows_btc)
             print('Discrepancy Found: BTC\n', 'Size:', rows_btc[0]['class'], '@', current_time,'\n')
-            moderate_discrepancy += 1
             continue
         elif margin_btc >= 0.2:
             minor_discrepancy += 1
@@ -216,7 +235,6 @@ def main():
         if margin_xrp >= 0.5:
             csv_row(rows_xrp)
             print('Discrepancy Found: XRP\n', 'Size:', rows_xrp[0]['class'], '@', current_time,'\n')
-            moderate_discrepancy += 1
             continue
         elif margin_xrp >= 0.2: 
             minor_discrepancy += 1
@@ -225,7 +243,6 @@ def main():
         if margin_ltc >= 0.5:
             csv_row(rows_ltc)
             print('Discrepancy Found: LTC\n', 'Size:', rows_ltc[0]['class'], '@', current_time,'\n')
-            moderate_discrepancy += 1
             continue 
         elif margin_ltc >= 0.2:
             minor_discrepancy += 1
@@ -234,7 +251,6 @@ def main():
         if margin_xlm >= 0.5:
             csv_row(rows_xlm)
             print('Discrepancy Found: XLM\n', 'Size:', rows_xlm[0]['class'], '@', current_time,'\n')
-            moderate_discrepancy += 1
             continue
         elif margin_xlm >= 0.2:
             minor_discrepancy =+1
@@ -243,12 +259,11 @@ def main():
         if margin_ada >= 0.5:
             csv_row(rows_ada)
             print('Discrepancy Found: ADA\n', 'Size:', rows_ada[0]['class'], '@', current_time,'\n')
-            moderate_discrepancy += 1
             continue
         elif margin_ada >= 0.2:
             minor_discrepancy += 1
             continue 
-        
+
         runs += 1
         
         if runs == 50:
